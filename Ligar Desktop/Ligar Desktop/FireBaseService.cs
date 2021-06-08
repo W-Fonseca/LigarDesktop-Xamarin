554c21b5bcff3e5ccf5bf01ee1851b4f6e959af2
Wellington Fonseca <wj_fonseca@hotmail.com>
2021-06-08 15:35:19 -0300

    LigarDestop-Xamarin
    LigarDestop-Xamarin

---
+++ Ligar Desktop/Ligar Desktop/FireBaseService.cs
@@ -0,0 +1,63 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ligar_Desktop.Services
+{
+    public class TextoBase
+    {
+        public string Power { get; set; }
+        public int Numero { get; set; }
+    }
+    public class FireBaseService
+    {
+        FirebaseClient firebase = new FirebaseClient("COLOQUE O LINK FIREBASE", //<<-- coloque o LINK do FIREBASE aqui
+          new FirebaseOptions
+          {
+              AuthTokenAsyncFactory = () => Task.FromResult("COLOQUE O TOKEN AQUI!") //<<-- COLOQUE O TOKEN AQUI
+          });
+
+        public async Task<List<TextoBase>> ObterTexto()
+        {
+            return (await firebase
+               .Child("Desktop")
+               .OnceAsync<TextoBase>()).Select(item => new TextoBase
+               {
+                   Power = item.Object.Power,
+                   Numero = item.Object.Numero
+               }).ToList();
+
+        }
+        public async Task Ligar()
+        {
+            var toUpdatePerson = (await firebase
+              .Child("Desktop")
+              .OnceAsync<TextoBase>()).Where(a => a.Object.Numero == 1).FirstOrDefault();
+
+            await firebase
+              .Child("Desktop")
+              .Child(toUpdatePerson.Key)
+              .PutAsync(new TextoBase { Numero = 1, Power = "ligado" });
+        }
+
+        public async Task Desligar()
+        {
+            {
+                var toUpdatePerson = (await firebase
+                  .Child("Desktop")
+                  .OnceAsync<TextoBase>()).Where(a => a.Object.Numero == 1).FirstOrDefault();
+
+                await firebase
+                  .Child("Desktop")
+                  .Child(toUpdatePerson.Key)
+                  .PutAsync(new TextoBase { Numero = 1,  Power = "desligado" });
+            }
+        }
+    }
+}
+
+    
---
+++ Ligar Desktop/Ligar Desktop/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Ligar_Desktop.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ligar_Desktop
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        FireBaseService firebaseService = new FireBaseService();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+
+            base.OnAppearing();
+            var texto = await firebaseService.ObterTexto();
+            lvtexto.ItemsSource = texto;
+        }
+
+        private async void btnLigar_Clicked(object sender, EventArgs e)
+        {
+            await firebaseService.Ligar();
+            var texto = await firebaseService.ObterTexto();
+            lvtexto.ItemsSource = texto;
+        }
+
+        private async void btnDesligar_Clicked(object sender, EventArgs e)
+        {
+            await firebaseService.Desligar();
+           var texto = await firebaseService.ObterTexto();
+            lvtexto.ItemsSource = texto;
+        }
+    }
+}
